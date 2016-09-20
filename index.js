9377e99a19e27cbfe6c23886288b22705a9155c0
michael hancock <michael.hancock@24.com>
2016-09-20 21:14:33 +0200

    support for rn 25+

--- index.js
+++ index.js
@@ -1,87 +1,91 @@
+/**
+ * @providesModule react-native-inviewport
+ */
 'use strict';
 
-var React = require('react-native');
-var window = React.Dimensions.get('window');
-var {View, NativeMethodsMixin} = React;
+import React from "react";
+import { View, NativeMethodsMixin, Dimensions} from "react-native";
+
+var window = Dimensions.get('window');
 
 module.exports = React.createClass({
-  displayName: 'InViewPort',
-  mixins: [NativeMethodsMixin],
-  propTypes: {
-    onChange: React.PropTypes.func.isRequired,
-    active: React.PropTypes.bool,
-    delay: React.PropTypes.number
-  },
+ displayName: 'InViewPort',
+ mixins: [NativeMethodsMixin],
+ propTypes: {
+   onChange: React.PropTypes.func.isRequired,
+   active: React.PropTypes.bool,
+   delay: React.PropTypes.number
+ },
 
-  getDefaultProps: function () {
-    return {
-      active: true,
-      delay: 100
-    };
-  },
+ getDefaultProps: function () {
+   return {
+     active: true,
+     delay: 100
+   };
+ },
 
-  getInitialState: function(){
-    return {
-      rectTop: 0,
-      rectBottom: 0
-    }
-  },
-  componentDidMount: function () {
-    if (this.props.active) {
-      this.startWatching();
-    }
-  },
+ getInitialState: function () {
+   return {
+     rectTop: 0,
+     rectBottom: 0
+   };
+ },
+ onLayout: function () {
+   if (this.props.active) {
+     this.startWatching();
+   }
+ },
 
-  componentWillUnmount: function () {
-    this.stopWatching();
-  },
+ componentWillUnmount: function () {
+   this.stopWatching();
+ },
 
-  componentWillReceiveProps: function (nextProps) {
-    if (nextProps.active) {
-      this.lastValue = null;
-      this.startWatching();
-    } else {
-      this.stopWatching();
-    }
-  },
+ componentWillReceiveProps: function (nextProps) {
+   if (nextProps.active) {
+     this.lastValue = null;
+     this.startWatching();
+   } else {
+     this.stopWatching();
+   }
+ },
 
-  startWatching: function () {
-    if (this.interval) { return; }
-    this.interval = setInterval(this.check, this.props.delay);
-  },
+ startWatching: function () {
+   if (this.interval) { return; }
+   this.interval = setInterval(this.check, this.props.delay);
+ },
 
-  stopWatching: function () {
-    this.interval = clearInterval(this.interval);
-  },
-  /**
-   * Check if the element is within the visible viewport
-   */
-  check: function () {
-    var el = this.refs.myview;
-    var rect = el.measure((ox, oy, width, height, pageX, pageY) => {
-      this.setState({
-        rectTop: pageY,
-        rectBottom: pageY + height,
-        rectWidth: pageX + width,
-      })
-    });
-    var isVisible = (
-      this.state.rectBottom != 0 && this.state.rectTop >= 0 && this.state.rectBottom <= window.height &&
-      this.state.rectWidth > 0 && this.state.rectWidth <= window.width
-    );
+ stopWatching: function () {
+   this.interval = clearInterval(this.interval);
+ },
+ /**
+  * Check if the element is within the visible viewport
+  */
+ check: function () {
+   var el = this.refs.myview;
+   var rect = el.measure((ox, oy, width, height, pageX, pageY) => {
+     this.setState({
+       rectTop: pageY,
+       rectBottom: pageY + height,
+       rectWidth: pageX + width,
+     });
+   });
+   var isVisible = (
+     this.state.rectBottom != 0 && this.state.rectTop >= 0 && this.state.rectBottom <= window.height &&
+     this.state.rectWidth > 0 && this.state.rectWidth <= window.width
+   );
 
-    // notify the parent when the value changes
-    if (this.lastValue !== isVisible) {
-      this.lastValue = isVisible;
-      this.props.onChange(isVisible);
-    }
-  },
+   // notify the parent when the value changes
+   if (this.lastValue !== isVisible) {
+     this.lastValue = isVisible;
+     this.props.onChange(isVisible);
+   }
+ },
 
-  render: function () {
-    return (
-      <View ref='myview' {...this.props}>
-        {this.props.children}
-      </View>
-    );
-  }
+ render: function () {
+   return (
+     <View ref='myview' {...this.props} onLayout={this.onLayout}>
+       {this.props.children}
+     </View>
+   );
+ }
 });
